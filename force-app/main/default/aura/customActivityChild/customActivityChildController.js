e15ae2cd86e22a007986406d8f8536c1f86a206a
Charles Law <charles.law@ideascience.com>
2020-03-11 23:21:57 +1100

    Revert "Merge branch 'Local' of https://github.com/clawai/lwc-recipes into Local"
    This reverts commit e5cdd4bb2e3d5192e53bc81e29cf65390eaa08f8, reversing
    changes made to 556702fe7d0ece7d69764c6f1df0de83fc13de1a.

--- force-app/main/default/aura/customActivityChild/customActivityChildController.js
+++
@@ -1,8 +0,0 @@
-({
-
-    toggleAcitivity: function (component, event, helper) {
-        // toggle 'slds-is-open' class to expand/collapse activity section
-        $A.util.toggleClass(component.find('expId'), 'slds-is-open');
-    },
-
-})
--- force-app/main/default/lwc/lwcOpportunitySectionDetails/lwcOpportunitySectionDetails.js
+++ force-app/main/default/lwc/lwcOpportunitySectionDetails/lwcOpportunitySectionDetails.js
@@ -18,7 +18,7 @@ export default class LwcOpportunitySectionDetails extends LightningElement {
     @api recordId;
     @api strTitle;
     @api recData;
-    @api opportunityRecordUiStr;
+
     fields = [NAME_FIELD, CLOSEDATE_FIELD];
 
     @wire(getRecordUi, {
@@ -28,116 +28,10 @@ export default class LwcOpportunitySectionDetails extends LightningElement {
     })
     opportunityRecordUi;
 
-
     get opportunityRecordUiStr() {
-        let key = 'sections';
-        let fieldsObj;
-        let sectionsObj = getValues(this.opportunityRecordUi.data, key);
-        let sectionObj = getSection(sectionsObj[0], 'Opportunity Information');
-        for (let a in sectionObj) {
-            fieldsObj = getFields(sectionObj[a]);
-
-            //            for (let b in sectionObj[a]) {
-            //               console.log(b);
-            //         }
-
-        }
-        //        let fieldsObj = getFields(sectionObj[0]);
-        let sectionStr = JSON.stringify(fieldsObj);
-        //console.log('sectionStr: ' + sectionStr);
-        return 'sectionStr';
-        /*
         return this.opportunityRecordUi ?
             JSON.stringify(this.opportunityRecordUi.data, null, 2) :
             '';
-    */
-
-        //return an array of values that match on a certain key
-        function getValues(obj, key) {
-            var objects = [];
-            //	console.log(Object.keys(obj).length);
-            for (var i in obj) {
-                if (!obj.hasOwnProperty(i)) {
-                    //                    console.log('***: ' + i);
-                    continue;
-                }
-
-                if (typeof obj[i] == 'object' && (i != key)) {
-                    //                    console.log('Second i : ' + i);
-                    objects = objects.concat(getValues(obj[i], key));
-                } else if (i == key) {
-                    console.log(' Found ' + i);
-                    objects.push(obj[i]);
-                    break;
-                }
-            }
-            return objects;
-        }
-
-        function getSection(obj, sectionName) {
-            var sectionObj = [];
-            for (let i in obj) {
-                for (let prop in obj[i]) {
-                    if (prop = 'heading') {
-                        if (obj[i][prop] == sectionName) {
-                            sectionObj.push(obj[i]);
-                            break;
-
-                        }
-                    }
-                }
-            }
-
-            //	const found = obj.find(element => element > 10);
-
-            return sectionObj;
-
-        }
-
-        function getFields(obj) {
-            var fieldsObj = [];
-            let setObj = new Set();
-            let layoutRowsObj = obj['layoutRows'];
-            console.log('HEAD: ' + layoutRowsObj[0]);
-            for (let newtemp in layoutRowsObj) {
-
-                for (let prop in layoutRowsObj[newtemp]) {
-                    //		console.log(layoutRowsObj[newtemp][prop][0]);
-                    //		console.log(layoutRowsObj[newtemp][prop][1]);
-
-                    for (let temp in layoutRowsObj[newtemp][prop]) {
-                        //			console.log('TEMP: ' + temp);
-                        for (let layoutItemsObj in layoutRowsObj[newtemp][prop][temp]) {
-                            //				console.log(temp + '---' + layoutItemsObj);
-                            if (layoutItemsObj == 'layoutComponents') {
-                                //console.log(layoutItemsObj);
-                                console.log(layoutRowsObj[newtemp][prop][temp][layoutItemsObj])
-                                for (let temp3 in layoutRowsObj[newtemp][prop][temp][layoutItemsObj]) {
-                                    console.log(layoutRowsObj[newtemp][prop][temp][layoutItemsObj][temp3])
-                                    for (let temp4 in layoutRowsObj[newtemp][prop][temp][layoutItemsObj][temp3]) {
-                                        console.log('TEMP4: ' + temp4);
-                                        if (temp4 == 'apiName') {
-                                            //fieldsObj.push(layoutRowsObj[newtemp][prop][temp][layoutItemsObj][temp3][temp4]);
-                                            setObj.add(layoutRowsObj[newtemp][prop][temp][layoutItemsObj][temp3][temp4]);
-                                            console.log('FOUNSD!!!');
-
-                                        }
-                                    }
-                                }
-
-                            }
-                        }
-                    }
-                }
-            }
-            fieldsObj = Array.from(setObj);
-            return fieldsObj;
-        }
-
-
-
-
     }
-
 
 }
